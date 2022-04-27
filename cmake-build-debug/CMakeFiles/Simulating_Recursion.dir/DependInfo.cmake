d1f8c4fa502262f7d7853e4c1169ee653b06d75a
Ranaihab <rana.ihab7@gmail.com>
2022-04-27 20:21:03 +0200

    initial

---
+++ cmake-build-debug/CMakeFiles/Simulating_Recursion.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "E:/University/College/Grade 3/Spring 2022/Concepts of Programming Language/Assignments/Simulating-Recursion/main.cpp" "E:/University/College/Grade 3/Spring 2022/Concepts of Programming Language/Assignments/Simulating-Recursion/cmake-build-debug/CMakeFiles/Simulating_Recursion.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Simulating_Recursion.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Simulating_Recursion.dir/main.cpp.obj"
+  "Simulating_Recursion.exe"
+  "Simulating_Recursion.exe.manifest"
+  "Simulating_Recursion.lib"
+  "Simulating_Recursion.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Simulating_Recursion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
